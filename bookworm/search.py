759be60cc469d5f23e589539d8f09b16c6729c31
sue-t-boyd <suetboyd@uw.edu>
2024-03-06 05:13:28 -0800

    Adding additional test cases for search

--- bookworm/search.py
+++ bookworm/search.py
@@ -1,3 +1,37 @@
+""" 
+Module with search functions accross several modalities.
+
+Includes helper functions as well as several search modes: 
+    (1) keyword search - based on TfidfVectorizer
+    (2) semantic search - hybird TfidfVectorizer/Voyeageai search
+    (3) plot_semantic search based on voyageai semantic search
+    (4) author2 search - fuzzy matching to author field 
+
+Functions in TestHelperFunctions Class
+==================================
+parse_genres(genre_str)
+    Parse genre dictionaries into keywords joined by commas
+
+fill_na(df)
+    Fill in missing values in dataframe with "unknown" or similar.
+
+preprocess_text(text)
+    Convert text to all lowercase.
+
+get_semantic_results
+    Extracts the indices of the closest books to given book_index.
+
+query_to_index
+
+
+Search Mode Functions
+=====================
+
+
+
+
+"""
+
 import os
 import ast
 import numpy as np
@@ -8,58 +42,115 @@ from thefuzz import fuzz
 from dotenv import load_dotenv
 import voyageai
 
-# Load environment variables from .env file
+# load environment variables from .env file
 load_dotenv()
 
-# API for embeddings voyage
+# load API for embeddings voyage
 api_key = os.getenv("API_KEY")
 vo = voyageai.Client(api_key=api_key)
 
+# load preprocessed distances/indices
 distances = np.load('bookworm/data/distances_updated.npy')
 indices = np.load('bookworm/data/indices_updated.npy')
 
 class HelperFunctions:
+
+    """
+    Helper Functions used by main search functions
+    """
+
     @staticmethod
     def parse_genres(genre_str):
+        """ 
+        Parse genre dictionaries into keywords joined by commas
+        
+        Paramters:
+            Genres: A dictinonary of genres.
+        Return
+            A string with genres expressed as keywords, separated
+            by commas.  
+        """
+
         try:
             genres_dict = ast.literal_eval(genre_str)
             genres_text = ', '.join(genres_dict.values())
         except (ValueError, SyntaxError):
             genres_text = 'Unknown Genre'
         return genres_text
-    
+
+
+    @staticmethod
     def fill_na(df):
-        df.fillna({'author': 'Unknown', 'book_title': 'Unknown', 
+        """ 
+        Fill in missing values in dataframe
+
+        Paramaters
+            df: A dataframe with fields author, book_title, genre
+                and summary. 
+        Return: 
+            A dataframe with missing values filled in. 
+        """
+
+        df.fillna({'author': 'Unknown', 'book_title': 'Unknown',
                    'genre': 'Unknown', 'summary': 'No Summary Available'},
-                     inplace=True)
+                    inplace=True)
         return df
 
     @staticmethod
     def preprocess_text(text):
+        """
+        Convert text to all lowercase.
+        
+        Parameters:
+            Text: A string
+        Returns
+            A lower case string
+        """
         text = str(text).lower()
         return text
 
     @staticmethod
     def get_semantic_results(book_index, num_books=10):
+        """
+        Extracts the indices of the closest books to given book_index.
+        
+        Indices extracted from global variable indices. Indices
+        variables assumed to reflect closest books based on
+        semantic search.
+        
+        Parameters: 
+            book_index: Int. The index of the book in the Indices 
+                            param that is the base of the search.
+            num_books:  Int. The number of indices to extract.
+        Returns: 
+            a numpy array of length num_books
+        """
         similar_books_indices = indices[book_index][:num_books]
         return similar_books_indices
 
     @staticmethod
     def query_to_index(df, query, vectorizer=None):
         df = HelperFunctions.fill_na(df)
-        df['combined_text'] = df.apply(lambda x: HelperFunctions.preprocess_text(f"{x['book_title']} {x['author']} {HelperFunctions.parse_genres(x['genre'])} {x['summary']}"), axis=1)
+        df["genre"] = df['genre'].apply(HelperFunctions.parse_genres)
+        df['combined_text'] = df.apply(lambda x:HelperFunctions.preprocess_text(
+            f"{x['book_title']} {x['author']} {(x['genre'])} {x['summary']}"),
+            axis=1)
 
         if vectorizer is None:
             vectorizer = TfidfVectorizer(stop_words='english')
             vectorizer.fit(df['combined_text'])
         query_vec = vectorizer.transform([query])
-        cosine_similarities = linear_kernel(query_vec, vectorizer.transform(df['combined_text'])).flatten()
+        cosine_similarities = linear_kernel(query_vec,
+                vectorizer.transform(df['combined_text'])).flatten()
         most_relevant_index = cosine_similarities.argsort()[-1]
         return most_relevant_index
 
 def keyword_search(df, query, num_books=10):
     df = HelperFunctions.fill_na(df)
-    df['combined_text'] = df.apply(lambda x: HelperFunctions.preprocess_text(f"{x['book_title']} {x['author']} {HelperFunctions.parse_genres(x['genre'])} {x['summary']}"), axis=1)
+    df['genre'] = df['genre'].apply(HelperFunctions.parse_genres)
+    df['combined_text'] = df.apply(lambda x: HelperFunctions.preprocess_text(
+        f"{x['book_title']} {x['author']} {(x['genre'])} {x['summary']}"), 
+        axis=1)
     query = HelperFunctions.preprocess_text(query)
     vectorizer = TfidfVectorizer(stop_words='english')
     tfidf_matrix = vectorizer.fit_transform(df['combined_text'])
@@ -74,9 +165,10 @@ def keyword_search(df, query, num_books=10):
 
 def semantic_search(df, query, num_books=10):
     book_index = HelperFunctions.query_to_index(df, query)
-    semantic_indices = HelperFunctions.get_semantic_results(book_index, 
+    semantic_indices = HelperFunctions.get_semantic_results(book_index,
                                                             num_books)
-    semantic_indices = semantic_indices.tolist() if isinstance(semantic_indices, np.ndarray) else semantic_indices
+    semantic_indices = semantic_indices.tolist() if \
+        isinstance(semantic_indices, np.ndarray) else semantic_indices
     results = df.loc[semantic_indices].head(num_books)
     return results
 
@@ -89,17 +181,23 @@ def author2_search(df, query, num_books=10):
     df['ratio'] = df.apply(calculate_ratio, axis=1)
     # filter the database to only those rows with match > ratio
     result = df[df["ratio"] > 80]
-    results = result.head(num_books)
+    results_sorted = result.sort_values(by='Book-Rating', ascending=False)
+    results = results_sorted.head(num_books)
     return results
 
 def plot_semantic_search(df, query, num_books = 10):
     # computing embeddings for the query
-    query_embedding = vo.embed(query, model="voyage-lite-02-instruct", input_type="document").embeddings
+    query_embedding = vo.embed(query, model="voyage-lite-02-instruct",
+                               input_type="document").embeddings
 
-    # Convert embeddings from string representation back to lists (and then to numpy arrays)
-    embeddings_matrix = np.array([ast.literal_eval(embedding) if isinstance(embedding, str) else embedding for embedding in df['embeddings']])
+    # Convert embeddings from string representation back to lists
+    # (and then to numpy arrays)
+    embeddings_matrix = np.array([ast.literal_eval(embedding) if
+            isinstance(embedding, str) else embedding for embedding
+            in df['embeddings']])
 
-    # Compute cosine similarities between the query embedding and the book embeddings
+    # Compute cosine similarities between the query embedding
+    #and the book embeddings
     similarities = cosine_similarity(query_embedding, embeddings_matrix)
 
     # Get indices of the top N similar books
--- bookworm/tests/test_search.py
+++ bookworm/tests/test_search.py
@@ -4,47 +4,172 @@ Module: test_search
 
 This module contains unit tests for the search module. 
 
-The tests are organized into a TestCase class named TestSear
+The tests are organized into two classes TestHelperFunctions and
+TestSearchFunctions
 
-Test Cases:
+Test Cases in TestHelperFunctions
+=================================
+test_preprocess_text(self):
+    Confirm preprocess_text function converts to lowercase
+
+test_parse_genres_empty(self):
+    Confirm empty genre field populated with "Unknown Genre".
+
+test_parse_genres_full(self):
+    Confirm proper parsing of genre dictionary.
+
+test_fill_na(self):
+    Confirm missing values in a df are correctly filled
+
+test_get_semantic_results(self):
+    Confirm that function extracts proper indices.
+
+test_query_to_index_smoke(self):
+    Confirm query_to_index function returns an np integer.
+
+test_query_to_index_calls_fill_na(self, mock_fill_na):
+    Confirm query_to_index correctly calls fill_na.
+
+test_query_exact(self):
+    Confirms that exact match queries return expected match.
+
+
+    
+    
 
 Dependencies:
 - unittest: The built-in unit testing framework in Python.
-- patch: A utility from the unittest.mock module used for mocking objects
-    during testing.
 - pandas: A library for data manipulation and analysis.
+- numpy: A library for numerical manipulation.
 
 Usage:
-Run this module to execute the unit tests for the search modules. 
+Run this module to execute the unit tests for the search module. 
 
 """
-
 import unittest
 from unittest.mock import patch
 import pandas as pd
+import numpy as np
+# pylint: disable=import-error
 from bookworm.search import HelperFunctions
 
+class TestHelperFunctions(unittest.TestCase):
+    """
+    Test cases for the Helper Functions in TestWrapper Module
+    """
+
+    def setUp(self):
+        """ 
+        Creates and loads teating data. 
+        """
+        f = "bookworm/data/test_data.csv"
+        self.test_dat = pd.read_csv(f)
+        self.test_dat_filled = HelperFunctions.fill_na(self.test_dat)
+
+        unfilled_data = {'author': ['Author1', None, 'Author3'],
+                'book_title': ['Book1', None, 'Book3'],
+                'genre': [None, 'Genre2', 'Genre3'],
+                'summary': ['Summary1', 'Summary2', None]}
+        self.test_dat2 = pd.DataFrame(unfilled_data)
 
 
-class TestSearch(unittest.TestCase):
-    """Test cases for the TestWrapper Module """
-    
     def test_preprocess_text(self):
+        """
+        Confirm preprocess_text function converts to lowercase
+        """
         results = HelperFunctions.preprocess_text("UPPERandlower74")
         self.assertEqual(results, "upperandlower74")
 
-    #def test_parse_genres_full(self):
-        #results = HelperFunctions.parse_genres()
-    
+
     def test_parse_genres_empty(self):
+        """
+        Confirm empty genre field populated with "Unknown Genre".
+        """
         results = HelperFunctions.parse_genres("")
         self.assertEqual(results, "Unknown Genre")
 
-    #def test_query_to_index(self):
-        #results = HelperFunctions.query_to_index
+    def test_parse_genres_full(self):
+        """
+        Confirm proper parsing of genre dictionary." 
+
+        Using book-ID = 4081 from test_data, expected result
+        is "Science Fiction, Speculative fiction".
+        """
+        df = self.test_dat
+        val_to_parse = df[df["book_id"] == 4081]["genre"][0]
+        results = HelperFunctions.parse_genres(val_to_parse)
+        expected = "Science Fiction, Speculative fiction"
+        self.assertEqual(results, expected)
 
 
-    
+    def test_fill_na(self):
+        """
+        Confirm missing values in a df are correctly filled
+        """
+
+        filled_df = HelperFunctions.fill_na(self.test_dat2)
+        self.assertEqual(filled_df.isnull().sum().sum(), 0)
+        self.assertEqual(filled_df['author'].iloc[1], 'Unknown')
+        self.assertEqual(filled_df['book_title'].iloc[1], 'Unknown')
+        self.assertEqual(filled_df['genre'].iloc[0], 'Unknown')
+        self.assertEqual(filled_df['summary'].iloc[2], 'No Summary Available')
+
+    def test_get_semantic_results(self):
+        """
+        Confirm that function extracts proper indices.
+        
+        get_semantic_results is a function that extracts the indices
+        of the closest books, based on semantic distance from a given
+        book identifed by index, from a file in which the indices are 
+        stored. Testing with existing indice_updated.npy data, 
+        ensure the proper row is extracted. For book_index 2, 
+        and num_books = 3, the function should 
+        return [11478, 7476, 10642]
+        
+        """
+        results = HelperFunctions.get_semantic_results(2, num_books=3)
+        expected = np.array([11478, 7476, 10642])
+        self.assertEqual(results.all(), expected.all())
+
+    def test_query_to_index_smoke(self):
+        """ 
+        Confirm query_to_index function returns an np integer.
+        """
+        query = "sample query"
+        index = HelperFunctions.query_to_index(self.test_dat, query)
+        self.assertIsInstance(index, np.int64)
+
+    @patch("bookworm.search.HelperFunctions.fill_na")
+    def test_query_to_index_calls_fill_na(self, mock_fill_na):
+        """ 
+        Confirm query_to_index correctly calls fill_na.
+        """
+        mock_fill_na.return_value = self.test_dat_filled
+        HelperFunctions.query_to_index(self.test_dat, "dog")
+        mock_fill_na.assert_called_once_with(self.test_dat)
+
+    def test_query_exact(self):
+        """
+        Confirms that exact match queries return expected match
+        
+        Pattern test. In any case where the query string is an exact 
+        and uniquematch to something in a given row of the test data, we
+        expect the relevant row to be returned as the match. 
+        """
+        for idx in [0, 10]:
+            for query in [self.test_dat["summary"][idx],
+                          self.test_dat["book_title"][idx]]:
+                expected = idx
+                result = HelperFunctions.query_to_index(self.test_dat, query)
+                self.assertEqual(result, expected)
+
+
+    # def test_select_search(self):
+    #     f = "bookworm/data/test_data.csv"
+    #     test_dat = pd.read_csv(f)
+    #     results = search_wrapper.select_search(test_dat, "Author2",
+    #                                            "J. R. Tolkien", 0.0, 0)
+    #     self.assertEqual(results.shape[0], 2)
 
 if __name__ == '__main__':
     unittest.main()
